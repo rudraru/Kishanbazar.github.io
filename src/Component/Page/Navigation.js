e671049f767068883b9eb89b74fafc77cbb1cef5
Rudra Kharel <aayushkharelpunk@gmail.com>
2023-04-27 23:12:22 +0545

    first commit

--- src/Component/Page/Navigation.js
+++ src/Component/Page/Navigation.js
@@ -1,33 +1,40 @@
+
+
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navigation.css'
+import './Navigation.css';
 
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState(prevState => ({ showMenu: !prevState.showMenu }));
+  }
+
   render() {
     return (
-      <nav className='nav'>
-        <ul className='Navitem'>
-       
-          <Link to='/' > Home</Link>
-      
-          <Link to='/Aboutus/us' > About us</Link>
-        
-         
-          <Link to='/fruits' > Vegetables</Link>
-          <Link to='/fruits' > Fruits </Link>
-          <Link to='/Contactus' > Contact us</Link>
-          <Link to='/fruits' > Cart</Link>
-          
+      <nav className="nav">
+        <div className="toggle-button" onClick={this.toggleMenu}>
+          <div className={this.state.showMenu ? "toggle-line active" : "toggle-line"}></div>
+          <div className={this.state.showMenu ? "toggle-line active" : "toggle-line"}></div>
+          <div className={this.state.showMenu ? "toggle-line active" : "toggle-line"}></div>
+        </div>
+        <ul className={this.state.showMenu ? "nav-items show-menu" : "nav-items"}>
+          <li className="nav-item"><Link to="/">Home</Link></li>
+          <li className="nav-item"><Link to="/aboutus/us">About Us</Link></li>
+          <li className="nav-item"><Link to="/vegetables">Vegetables</Link></li>
+          <li className="nav-item"><Link to="/fruits">Fruits</Link></li>
+          <li className="nav-item"><Link to="/contactus">Contact Us</Link></li>
+          <li className="nav-item"><Link to="/cart">Cart</Link></li>
         </ul>
-        <form >
-        <div className="search-box">
-  <input type="text" placeholder="Search..."/>
-  <button type="submit">Search</button>
-
-
-
-
-</div>
+        <form className="search-box">
+          <input type="text" placeholder="Search..." />
+          <button type="submit">Search</button>
         </form>
       </nav>
     );
--- src/Component/Page/Signup.jsx
+++ src/Component/Page/Signup.jsx
@@ -1,50 +1,83 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import createUser from '../api/Api';
+import './Signup.css';
 
-
-
-import React, { useState} from 'react';
-import createUser  from '../api/Api';
 const Signup = () => {
   const [users, setUsers] = useState([]);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     const data = await getUsers();
-  //     setUsers(data);
-  //   };
-
-  //   fetchUsers();
-  // }, []);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const history = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // Check if email already exists
+    const existingUser = users.find(user => user.email === email);
+    if (existingUser) {
+      setError('Email is already taken');
+      setTimeout(() => {
+        setError('');
+      }, 4000);
+      return;
+    }
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      setTimeout(() => {
+        setError('');
+      }, 4000);
+      return;
+    }
+
     const newUser = await createUser(name, email, password);
     setUsers(prevUsers => [...prevUsers, newUser]);
     setName('');
     setEmail('');
     setPassword('');
+    setConfirmPassword('');
+    setSuccess(true);
+
+    setTimeout(() => {
+      setSuccess(false);
+      history('/');
+    }, 4000);
   };
 
   return (
-    <div>
-      <h1>Users:</h1>
+    <div className="signup-container">
+      <h1>Signup</h1>
+
+      {error && <p className="error">{error}</p>}
+
+      {success && (
+        <div className="success-popup">
+          <p>Signup successful! Redirecting to home page...</p>
+        </div>
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" value={name} onChange={event => setName(event.target.value) } required />
+        <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} required />
+        <input type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} required/>
+        <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={event => setConfirmPassword(event.target.value)} required/>
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* <h2>Users:</h2>
       <ul>
         {users.map(user => (
           <li key={user.id}>{user.name} - {user.email}</li>
         ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name" value={name} onChange={event => setName(event.target.value)} />
-        <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} />
-        <button type="submit">Submit</button>
-      </form>
+      </ul> */}
     </div>
   );
 };
 
 export default Signup;
-
 
