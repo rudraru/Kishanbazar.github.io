bda578c33a1c87e8219f47b7e56dce411c5e0be8
Rudra Kharel <aayushkharelpunk@gmail.com>
2023-05-08 12:40:14 +0545

    Appchanges

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import Signup from './Component/Page/Signup';
 import Login from './LogIn';
 
 import FruitList from './Component/Tab';
-import VideoBackground from './Component/Page/Videobackground';
+
 import AboutUs from './Component/Page/Aboutus';
 import ContactUs from './Component/Page/Contactus';
 import Homepage from './Component/Page/Home';
 import FruitDetails from './Component/Page/FruitsDetails';
-import BackgroundImage from './Component/Page/BackgroundImage';
+
 import SummaryPage from './Component/Page/Summary';
 import Bcart from './Component/Page/Bcart';
 import TermsAndConditionsPage from './Component/Page/Tac';
@@ -21,8 +21,7 @@ function App() {
   return (
     <>
  <Navigation/>
- {/* <VideoBackground/> */}
- {/* <BackgroundImage image='https://wallpapercave.com/wp/wp8558453.jpg'/> */}
+
 <Routes>
 <Route exact path="/" element={<Homepage/>} />
  
